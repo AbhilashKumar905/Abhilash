53e934713a18120771c38b58368b42e10160ba55
Abhilash <abhilashkumar905@gmail.com>
2023-08-30 19:16:40 +0530

    hello

---
+++ BankTest1.java
@@ -0,0 +1,15 @@
+package interfacedemo;
+
+public class BankTest1 
+{
+
+	public static void main(String[] args) 
+	{
+        //Bank b1 =10;
+		//Bank b1 =true;
+		//Bank b1 =new Bank;
+		  Bank b1 = null;
+
+	}
+
+}
